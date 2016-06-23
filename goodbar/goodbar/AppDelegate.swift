eae9f3e24254b60b649fe90d7843f4da78e978a8
Peter Hajas <peterhajas@gmail.com>
2016-06-22 20:03:02 -0700

    Add update functionality

--- goodbar/goodbar/AppDelegate.swift
+++ goodbar/goodbar/AppDelegate.swift
@@ -13,13 +13,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     let configFileLoader = ConfigurationFileLoader()
     var barView: BarView? = nil
+    var updateTimer: NSTimer? = nil
     
     func applicationDidFinishLaunching(notification: NSNotification) {
+        updateTimer = NSTimer(timeInterval: 1, target: self, selector: #selector(AppDelegate.update), userInfo: nil, repeats: true)
+        NSRunLoop.mainRunLoop().addTimer(updateTimer!, forMode: NSRunLoopCommonModes)
+        
         configFileLoader.loadConfigurationFile { (leftSegment, centerSegment, rightSegment) in
             self.barView = BarView(leftSegment: leftSegment, centerSegment: centerSegment, rightSegment: rightSegment)
             self.window.contentView = self.barView
-            
-            self.barView?.updateBarContents()
+            self.update()
         }
+    }
+    
+    func update() {
+        self.barView?.updateBarContents()
     }
 }
